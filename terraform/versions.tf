1a65512825c22f1b30510f3eb859252185fddb92
Riyasarab <riyabiju04@gmail.com>
2026-09-26 01:03:16 +1000

    Configure Azure backend for Terraform state

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.7.0"
 
+  backend "azurerm" {
+    resource_group_name  = "sit722-tfstate-rg"
+    storage_account_name = "riya10dstate260925"
+    container_name       = "tfstate"
+    key                  = "week08.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
